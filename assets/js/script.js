3f68c75442bc2a85a27267bd593f8b0b529ef9de
Yeasir H <yeasir01@gmail.com>
2020-02-09 00:53:05 -0800

    added addtinal functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,41 @@
 $("#search-btn").on("click", function (event) {
-    
+
     event.preventDefault();
 
     var search = $('#search-txt').val();
+    const dayQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&units=imperial&appid=bed785ee913d61642b01b96cd98d7b6d";
 
-    var query = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&units=imperial&appid=bed785ee913d61642b01b96cd98d7b6d";
 
     $.ajax({
-        url: query,
+        url: dayQuery,
         method: "GET"
     }).then(function (response) {
-        $("#current-city").text(response.name + ', ' + response.sys.country);
-        $("#current-temp").text(response.main.temp);
-        $("#current-humidity").text(response.main.humidity);
-        $("#current-windspeed").text(response.wind.speed);
+        $("#current-city").text(response.name + ' (' + response.sys.country + ')');
+        $("#current-temp").text(response.main.temp + ' F°');
+        $("#current-humidity").text(response.main.humidity + "%");
+        $("#current-windspeed").text(response.wind.speed + " MPH");
         $("#current-uv").text(response.sys.id);
+
+        const iconLink = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+
+        $("#weather-icon").html('<img src=' + iconLink + ' class="current-icon" alt="Weather icon"></img>');
+
+        fiveDay(search);
     })
 
 });
+
+function fiveDay(search) {
+
+    const fiveQuery = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&units=imperial&appid=bed785ee913d61642b01b96cd98d7b6d"
+
+    $.ajax({
+        url: fiveQuery,
+        method: "GET"
+    }).then(function (response) {
+        $("#day-one-date").text(response.list[0].dt);
+        $("#day-one-temp").text('Temp: ' + response.list[0].main.temp + ' F°');
+        $("#day-one-humid").text('Humidity: ' + response.list[0].main.humidity + '%');
+
+    })
+};
